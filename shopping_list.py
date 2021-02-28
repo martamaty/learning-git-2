d3260d321ba5062be0f11328e275002344526905
Marta Matyjaszczyk <marta.maty.gdn@gmail.com>
2021-02-28 15:43:13 +0100

    sum

--- shopping_list.py
+++ shopping_list.py
@@ -6,3 +6,10 @@ dicto={
 }
 for i in dicto:
     print("jezeli ide do",i,"kupuje",dicto[i])
+
+count = 0
+for key, value in dicto.items(): 
+    if isinstance(value, list):
+        count += len(value) 
+
+print("w sumie to kupuje",count,"produktow")    
